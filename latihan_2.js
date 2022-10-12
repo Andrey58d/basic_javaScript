0b7fcbf67c6a1ec82421511722f06679871c6f54
andre <andreydre57@gmail.com>
2022-10-12 11:22:39 +0700

    latihan 2 for 11-10-22

---
+++ latihan_2.js
@@ -0,0 +1,37 @@
+//var color= 2;
+/*
+1 like 2 dark 3 noktur 4 terminal 5 indigo
+*/
+/*switch(color){
+    case 1:
+        console.log("like")
+        break;
+    case 2:
+        console.log("dark")
+        break
+    case 3:
+        console.log("noktur")
+        break
+    case 4:
+        console.log("terminal")
+        break
+    default:
+        console.log("indigo")
+        break\
+}
+    */
+
+//your code color
+/*{
+console.log(color == 1 ? "light" :
+(color == 2 ? "dark" :
+(color == 3 ? "nocturne" :
+color == 4 ? "terminal" : "indigo")))
+}*/
+
+for(i=3; i>=1; i--) {
+    console.log(i);
+}
+
+
+    
